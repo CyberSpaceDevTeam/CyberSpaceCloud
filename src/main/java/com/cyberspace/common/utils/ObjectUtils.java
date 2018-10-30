08d1bbfe40b57bbf14bdcb9a8114d63e02b3381a
Justdwiwt <len_master@hotmail.com>
2018-10-30 23:53:26 +0800

    update all static resources

---
+++ src/main/java/com/cyberspace/common/utils/ObjectUtils.java
@@ -0,0 +1,14 @@
+package com.cyberspace.common.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * static mapper
+ *
+ * @author Justdwiwt
+ */
+public class ObjectUtils {
+
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+}
--- src/main/java/com/cyberspace/cyber/CyberApplication.java
+++ src/main/java/com/cyberspace/cyber/CyberApplication.java
@@ -2,11 +2,18 @@ package com.cyberspace.cyber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class CyberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CyberApplication.class, args);
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String test() {
+        return "index";
     }
 }
